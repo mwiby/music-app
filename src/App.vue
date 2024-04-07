63ecf85862ab9ce001d09615bb2698895538e670
Markus Byremo <markuswb77@gmail.com>
2024-04-07 14:43:15 +0200

    base setup for styling

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,17 @@
 </script>
 
 <template>
-
+  <div>
+    <div class="container">
+      <i class="chevron-up"></i>
+      <i class="chevron-down"></i>
+      <i class="chevron-left"></i>
+      <i class="chevron-right"></i>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "@/assets/styles.scss";
 
 </style>
